190efa30fdd6ad5307f46687732427cffa298154
SerhiiChudnivets <SerhiiChudnivets@users.noreply.github.com>
2026-07-10 12:03:12 +0000

    Update from PostMax - 2026-07-10T12:03:12.918Z

--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -9,6 +9,7 @@ import BlogTemplate from '../templates/BlogTemplate'
 import LandingTemplate from '../templates/LandingTemplate'
 import MinimalTemplate from '../templates/MinimalTemplate'
 import FullWidthTemplate from '../templates/FullWidthTemplate'
+import HomepageTemplate from '../templates/HomepageTemplate'
 
 interface MediaFile {
   id?: number
@@ -33,6 +34,7 @@ interface PageData {
   seo_title?: string
   seo_description?: string
   html_head?: string
+  htmlHead?: string
   hero_title?: string
   hero_subtitle?: string
   hero_badge?: string
@@ -40,7 +42,7 @@ interface PageData {
   cta_text?: string
   cta_link?: string
   sections?: ContentSection[]
-  template?: 'default' | 'blog' | 'landing' | 'minimal' | 'full-width'
+  template?: 'default' | 'blog' | 'landing' | 'minimal' | 'full-width' | 'homepage'
 }
 
 interface SiteData {
@@ -57,6 +59,7 @@ interface SiteData {
   pages?: PageData[]
   header_menu?: any[]
   footer_menu?: any[]
+  [key: string]: any
 }
 
 const templates = {
@@ -65,6 +68,7 @@ const templates = {
   landing: LandingTemplate,
   minimal: MinimalTemplate,
   'full-width': FullWidthTemplate,
+  homepage: HomepageTemplate,
 }
 
 export default function DynamicPage({ page, site }: { page: PageData; site: SiteData }) {
@@ -109,24 +113,68 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
     }
   }
 
+  const site = {
+    name: data.name,
+    url: data.url,
+    site_name: data.site_name,
+    accent_color: data.accent_color,
+    footer_text: data.footer_text,
+    allow_indexing: data.allow_indexing,
+    logo: data.logo,
+    login_text: data.login_text,
+    register_text: data.register_text,
+    redirect_link: data.redirect_link,
+    pages: data.pages,
+    header_menu: data.header_menu,
+    footer_menu: data.footer_menu,
+    hero_title: data.hero_title,
+    heroTitle: data.heroTitle,
+    hero_subtitle: data.hero_subtitle,
+    heroSubtitle: data.heroSubtitle,
+    hero_badge: data.hero_badge,
+    hero_image: data.hero_image,
+    heroImage: data.heroImage,
+    breadcrumbs: data.breadcrumbs,
+    home_name: data.home_name,
+    cta_text: data.cta_text,
+    cta_background: data.cta_background,
+    tagline: data.tagline,
+    features_list: data.features_list,
+    content: data.content,
+    html_head: data.html_head,
+    htmlHead: data.htmlHead,
+    seo_title: data.seo_title,
+    seoTitle: data.seoTitle,
+    seo_description: data.seo_description,
+    seoDescription: data.seoDescription,
+    popup_text: data.popup_text,
+    faq_title: data.faq_title,
+    faqTitle: data.faqTitle,
+    faq: data.faq,
+    FAQ: data.FAQ,
+    slots_title: data.slots_title,
+    bonus_title: data.bonus_title,
+    get_bonus_btn_text: data.get_bonus_btn_text,
+    main_background: data.main_background,
+    secondary_background: data.secondary_background,
+    button_background: data.button_background,
+    button_text: data.button_text,
+    text_color: data.text_color,
+    color_highlight_text: data.color_highlight_text,
+    color_main_btn_text: data.color_main_btn_text,
+    Slots: data.Slots,
+    Bonuses: data.Bonuses,
+    main_background_img: data.main_background_img,
+    popup_logo: data.popup_logo,
+    footer_images: data.footer_images,
+    footerImages: data.footerImages,
+    faq_schema: data.faq_schema,
+  }
+
   return {
     props: {
       page,
-      site: {
-        name: data.name,
-        url: data.url,
-        site_name: data.site_name,
-        accent_color: data.accent_color,
-        footer_text: data.footer_text,
-        allow_indexing: data.allow_indexing,
-        logo: data.logo,
-        login_text: data.login_text,
-        register_text: data.register_text,
-        redirect_link: data.redirect_link,
-        pages: data.pages,
-        header_menu: data.header_menu,
-        footer_menu: data.footer_menu
-      }
+      site: Object.fromEntries(Object.entries(site).filter(([, value]) => value !== undefined))
     }
   }
 }
